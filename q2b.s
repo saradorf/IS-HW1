45cc6f636c251814c5b7c55297ffcd178631d5bc
saradorf <saradorfman@mail.tau.ac.il>
2024-05-30 20:27:32 +0300

    q2b - working

--- q2b.s
+++ q2b.s
@@ -14,3 +14,44 @@ my_function:
     # TODO:
     # 1. Read the input to the function from the stack.
     # 2. Save the result in the register EAX (and then return!).
+    PUSH EBP
+    MOV EBP, ESP
+    PUSH EBX
+    PUSH EDX
+    PUSH ECX
+
+    # Read the input to the function from the stack (n)
+    MOV EBX, [EBP + 8]
+
+    # Base case: n = 0 (or n < 0)
+    MOV EAX, 0
+    CMP EBX, 0
+    JLE END
+
+    # Base case: n = 1
+    MOV EAX, 1
+    CMP EBX, 1
+    JE END
+
+    # fibonnaci loop
+    MOV ECX, 2
+    MOV EDX, 1
+    MOV EAX, 1
+    LOOP:
+        CMP ECX, EBX
+        JGE END
+        PUSH EAX
+        IMUL EAX, EAX
+        IMUL EDX, EDX
+        ADD EAX, EDX
+        POP EDX
+        INC ECX
+        JMP LOOP
+
+    END:
+        POP ECX
+        POP EDX
+        POP EBX
+        MOV ESP, EBP
+        POP EBP
+        RET
